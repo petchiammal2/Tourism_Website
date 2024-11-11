f38cf361a5aff88fbe2a2a364377c75d1bd7959f
Rohi78960 <rohinivkp2@gmail.com>
2024-11-11 15:33:31 +0530

    testing
    testing

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ function App()
   return (
     <div className="App">
 
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
+      {/* </nav> */}
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Home/>}/>
         <Route path="about" element={<About/>}/>
         <Route path="contact" element={<Contact/>}/>
         <Route path="blog" element={<Blog/>}/>
         <Route path="layout" element={<Layout/>}/>
         <Route path="gallery" element={<Gallery/>}/>
-      </Routes>
-     */}
+      </Routes> */}
+     {/* */} */}
       <Footer />
 
 
@@ -40,3 +40,4 @@ function App() {
 }
 
 export default App;
+ 
