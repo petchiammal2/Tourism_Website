cb51fab68cb3ad604bac9cc94e62911d03acadd2
Rohi78960 <rohinivkp2@gmail.com>
2024-11-12 10:32:07 +0530

    testing
    testing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-
+import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home"
-import About from "./components/About"
+// import Home from "./components/Home"
+// import About from "./components/About"
 import Blog from "./components/Blog"
-import Contact from "./components/Contact"
-import Layout from "./components/Layout"
-import Gallery from "./components/Gallery"
+// import Contact from "./components/Contact"
+// import Layout from "./components/Layout"
+// // import Gallery from "./components/Gallery"
 import './App.css'
-import Footer from "./components/Footer"
+// import Footer from "./components/Footer"
 
 
 function App() {
   return (
     <div className="App">
+      <Blog/>
   
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        {/* <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} /> */}
+        {/* <Route path="contact" element={<Contact />} /> */}
         <Route path="blog" element={<Blog />} />
-        <Route path="layout" element={<Layout />} />
-        <Route path="gallery" element={<Gallery />} />
+        {/* <Route path="layout" element={<Layout />} /> */}
+        {/* <Route path="gallery" element={<Gallery />} /> */}
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 
