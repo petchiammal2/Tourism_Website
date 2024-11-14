5556ae2a5de87f08d255c1009222a6e4d8fbb6ba
Rohi78960 <rohinivkp2@gmail.com>
2024-11-15 00:29:07 +0530

    testing
    testing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 // import Home from "./components/Home"
 // import About from "./components/About"
-import Blog from "./components/Blog"
 // import Contact from "./components/Contact"
 // import Layout from "./components/Layout"
 // // import Gallery from "./components/Gallery"
 import './App.css'
+import HeroSection from "./Components/Herosection";
+import Destinations from "./Components/Destinations";
 // import Footer from "./components/Footer"
 
 
 function App() {
   return (
     <div className="App">
+      <HeroSection/>
+      <Destinations/>
 
   
-      <Routes>
+      {/* <Routes> */}
         {/* <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} /> */}
         {/* <Route path="contact" element={<Contact />} /> */}
-        <Route path="blog" element={<Blog />} />
+        {/* <Route path="blog" element={<Blog />} /> */}
         {/* <Route path="layout" element={<Layout />} /> */}
         {/* <Route path="gallery" element={<Gallery />} /> */}
-      </Routes>
+      {/* </Routes> */}
       {/* <Footer /> */}
     </div>
   );
