de8d061fed9538770fbd9d8f33d50fc68c9bef5e
Jenish000007 <jenish.m0708@gmail.com>
2024-11-14 10:13:28 +0530

    corrections

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-<<<<<<< Updated upstream
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home"
-import About from "./components/About"
-import Blog from "./components/Blog"
-import Contact from "./components/Contact"
-import Layout from "./components/Layout"
-import Gallery from "./components/Gallery"
-import './App.css'
-import Footer from "./components/Footer"
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Layout from "./components/Layout";
+import Gallery from "./components/Gallery";
+import './App.css';
+import Footer from "./components/Footer";
 import Header from "./components/Header";
-=======
-import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StayPackage from './components/StayPackage';
->>>>>>> Stashed changes
-
 
 function App() {
   return (
-<<<<<<< Updated upstream
     <div className="App">
-
-  <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
@@ -32,14 +24,9 @@ function App() {
         <Route path="gallery" element={<Gallery />} />
       </Routes>
       <Footer />
+      <StayPackage />
     </div>
-=======
-    <StayPackage/>
-
->>>>>>> Stashed changes
   );
-
 }
 
 export default App;
- 
