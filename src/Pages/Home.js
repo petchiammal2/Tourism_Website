587f40058bfecb20c6ff0d5b63151bf9fd363961
Rohi78960 <rohinivkp2@gmail.com>
2024-11-23 22:25:25 +0530

    testing
    testing

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,20 @@
 import Section1 from "../Components/Section1";
 import HomeSection1 from "../Components/HomeSection1";
 import Destinations from "../Components/Destinations";
+import Explore from "../Components/Explore";
+
+
+
 const Home = () => {
     return (
    <>
+
       <Section1/>
-      
       <HomeSection1/>
       <Destinations/>
+      <Explore/>
+      
+      
     
    </>
      
