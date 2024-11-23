012ac83ce1f57db54541dc3e87e8b57cbdcca81a
petchiammal2 <kalaiesakki410@gmail.com>
2024-11-22 20:35:52 -0800

    Gallery Page
    This is New Gallery Page.

--- src/Components/GalleryHero.js
+++ src/Components/GalleryHero.js
@@ -1,18 +1,17 @@
 import React from "react";
 import './CSS/GalleryHero.css';
-import Image1 from '../Assets/gallery-7.jpg';  // Corrected path
-
-
+import Image1 from '../Assets/pic_1.webp';  // Corrected path
 
 const GalleryHero = () => {
   return (
     <div className="gallery-hero">
-      <img src={Image1} alt="Bible background" className="gallery-image"/>
+      <img src={Image1} alt="Bible background" className="gallery-img"/>
       <div className="gallery-text">
         <h1>Gallery</h1>
         <p>
-          Explore the moments of joy , entertainment in our photo gallery.
-          These images capture the spirit of our various tourism places in the TamilNadu.
+          Explore moments of joy, worship, and community in our photo gallery.
+          These images capture the spirit of our church family and the various
+          events and activities we've shared.
         </p>
       </div>
     </div>
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,8 +1,16 @@
 import GalleryHero from "../Components/GalleryHero";
+import GallerySection1 from  "../Components/GallerySection1"
+import GallerySection2 from "../Components/GallerySection2";
+import GallerySection3  from "../Components/GallerySection3";
 const Gallery = () => {
     return (
       <>
       <GalleryHero />
+      <GallerySection1 />
+      <GallerySection2 />
+      <GallerySection3 />
+
+
       
       </>
     )
